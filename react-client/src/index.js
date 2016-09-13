121c7a520537258c7dff3cd4b535f7d217d52489
sqq <fyszsqq@163.com>
2016-09-13 13:45:34 +0800

    react客户端渲染实践

---
+++ react-client/src/index.js
@@ -0,0 +1,2 @@
+require('./style.scss');
+document.write('<div>你是这个世界上无与伦比的美丽</div>');
---
+++ react-client/webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        app: path.join(__dirname, 'src'),
+        vendors: ['react', 'redux']
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                loaders: ["style", "css", "sass"]
+            },
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+    ]
+};
